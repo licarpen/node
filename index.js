4d690dc68745456cf2db9605bafb0b6b5f2dd81b
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-01 21:43:17 -0800

    through page 24

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const server = require('./server.js');
 const router = require('./router.js');
+const requestHandlers = require('./request-handlers.js');
 
-server.start(router.route);
+const handle = {
+    '/': requestHandlers.start,
+    '/start': requestHandlers.start,
+    '/upload': requestHandlers.upload
+};
+
+server.start(router.route, handle);
