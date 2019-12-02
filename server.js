306b9cc2ac1a55488034ac9048c5bc05967c74f7
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-01 21:22:55 -0800

    through page 18

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require('http');
+const url = require('url');
 
-function onRequest(request, response){
-    console.log('Request received');
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello, world!');
-    response.end();
-};
+function start(route) {
+    function onRequest(request, response){
+        const pathname = url.parse(request.url).pathname;
+        console.log(`Request for ${pathname} received.`);
 
-http.createServer(onRequest).listen(8888);
-console.log('Server started');
+        route(pathname);
+
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello, world!');
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log('Server started');
+}
+
+exports.start = start;
